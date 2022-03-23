62db355e787dbf4a1ce2c123770722cd52b6b042
ClementDINSSET <clementdubois73@gmail.com>
2022-03-23 15:24:08 +0100

    1

--- src/UPJV/Validator/EstValide.php
+++ src/UPJV/Validator/EstValide.php
@@ -1,14 +1,35 @@
 <?php
 
+/**
+ * Vérifie que la taille de la chaine comporte au moins 2 caractères
+ */
+
 namespace UPJV\Validator;
 
+/**
+ * Class Min implémente les méthode build & check
+ */
 class EstValide implements ValidatorInterface
 {
+	/**
+     * Récupère la taille minimum spécifier dans la conf
+     *
+     * @param array $param
+     *
+     * @return object
+     */
     public function build(array $param): object
     {
         return $this;
     }
 
+	/**
+     * Test la longueur de la chaine de caractères
+     *
+     * @param $input
+     *
+     * @return bool
+     */
     public function check($input): bool
     {
         return true;
